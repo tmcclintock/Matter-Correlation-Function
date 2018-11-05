f6d997d222e7a5a4c978637b8871a52e37cef4f9
Tom McClintock <tmcclintock89@gmail.com>
2018-11-05 11:07:23 -0500

    Working on the emulator.

--- ximm/__init__.py
+++ ximm/__init__.py
@@ -9,3 +9,9 @@ ln_xi_stddev = 1.609
 
 #Number of principle components
 Npc = 12
+
+#Load in all the data we need, which includes pre-computed matrices
+r             = np.loadtxt("radii.txt")
+scale_factors = np.load("scale_factors.npy")
+ln_xi_mean    = np.load("ln_xi_mean.npy")
+phis          = np.load("phis.npy")
